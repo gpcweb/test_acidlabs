1fef450cfcfb5d6055d921fa93af53c39a8e0e8e
Gabriel Poblete <poblete.cuadra@live.cl>
2014-09-03 15:05:54 -0400

    root

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
     match ':controller(/:action(/:id))', :via => [:get, :post]
 
+    get '/' => "rails/search#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
